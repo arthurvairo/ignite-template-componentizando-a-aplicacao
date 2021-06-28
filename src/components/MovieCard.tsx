aa16c6f27d0c668d066eb605524006e79186d911
arthurvairo <cafesafa72@gmail.com>
2021-06-28 16:24:02 -0300

    feat: pull the functions of app on content and side bar

--- src/components/MovieCard.tsx
+++ src/components/MovieCard.tsx
@@ -11,26 +11,29 @@ interface MovieCardProps {
 
 export function MovieCard(props: MovieCardProps) {
   return (
-    <div className="movie-card">
-      <img
-        src={props.poster}
-        alt={props.title}
-      />
+    <div className="card-wrapper">
+      <a href="https://google.com" target="_blank">
+        Assistir Filme
+      </a>
 
-      <div>
-        <div className="movie-info">
-          <span>{props.title}</span>
-          <div className="meta">
-            <div>
-              <Star /> {props.rating}
-            </div>
+      <div className="movie-card">
+        <img src={props.poster} alt={props.title} />
 
-            <div>
-              <Clock /> {props.runtime}
+        <div>
+          <div className="movie-info">
+            <span>{props.title}</span>
+            <div className="meta">
+              <div>
+                <Star /> {props.rating}
+              </div>
+
+              <div>
+                <Clock /> {props.runtime}
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
